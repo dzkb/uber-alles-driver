38f95f9a9d9e8b1e0e63907865f9c416952a9944
Tomek <tomasz.kaiser@gmail.com>
2017-04-21 17:40:48 +0200

    Pierwsze layouty - layout logowania, layout kierowcy z menu w postaci tabów, fragmenty - historia, aktywne, ustawienia

--- app/src/main/java/com/example/szymon/app/LoginActivity.java
+++ app/src/main/java/com/example/szymon/app/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.szymon.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.login_button)
+    public void onLoginButtonClick(View v) {
+        Intent intent = new Intent(LoginActivity.this, DriverActivity.class);
+        startActivity(intent);
     }
 }
