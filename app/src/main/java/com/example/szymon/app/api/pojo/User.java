1f4f2eea7a6eef587a43be1f0e4f508d81cca5bf
simon <szymon.zwolinski95@gmail.com>
2017-05-06 15:05:53 +0200

    nowe klasy modelowe

---
+++ app/src/main/java/com/example/szymon/app/api/pojo/User.java
@@ -0,0 +1,14 @@
+package com.example.szymon.app.api.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("phoneNumber")
+    public String phoneNumber;
+    @SerializedName("firstName")
+    public String firstName;
+    @SerializedName("lastName")
+    public String lastName;
+
+}
