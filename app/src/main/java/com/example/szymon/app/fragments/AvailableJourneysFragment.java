2567784df2449dc4408859334d2f31c5f43747dd
Dzakub <dzakub@gmail.com>
2017-05-15 15:13:46 +0200

    [2] Dodana obsługa pakietu CMFareRequest

--- app/src/main/java/com/example/szymon/app/fragments/AvailableJourneysFragment.java
+++ app/src/main/java/com/example/szymon/app/fragments/AvailableJourneysFragment.java
@@ -1,16 +1,20 @@
 package com.example.szymon.app.fragments;
 
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.szymon.app.R;
 import com.example.szymon.app.RecyclerAdapter;
@@ -18,7 +22,11 @@ import com.example.szymon.app.api.pojo.CMFareRequest;
 import com.example.szymon.app.api.pojo.Fare;
 import com.example.szymon.app.api.pojo.Point;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import static com.example.szymon.app.LogedUserData.ALL_FARES_LIST;
 
@@ -60,25 +68,45 @@ public class AvailableJourneysFragment extends Fragment {
 
         bottomSheetBehavior = BottomSheetBehavior.from(rootView.findViewById(R.id.bottomSheetLayout));
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-        openBottomSheet(rootView, bottomSheetBehavior);
 
         return rootView;
     }
 
-    private void openBottomSheet(View rootView, final BottomSheetBehavior bottomSheetBehavior) {
+    private String getTimeFromDate(String date) {
+        return date.substring(11, 16);
+    }
+
+    private String findLocation(Point point) {
+        Geocoder geocoder;
+        List<Address> addresses = null;
+        geocoder = new Geocoder(rootView.getContext(), Locale.getDefault());
+        double startPointLatitude = point.getLatitude();
+        double startPointLongitude = point.getLongitude();
+        try {
+            addresses = geocoder.getFromLocation(startPointLatitude, startPointLongitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+
+        return address.length() >= 20 ? address.substring(0, 17) + "..." : address;
+    }
+
+    private void openBottomSheet(View rootView, final BottomSheetBehavior bottomSheetBehavior, CMFareRequest fareRequest) {
         fareAcceptBtn = (Button) rootView.findViewById(R.id.fare_accept_btn);
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        TextView startPoint = (TextView) rootView.findViewById(R.id.bottom_sheet_start_point_name);
+        TextView endPoint = (TextView) rootView.findViewById(R.id.bottom_sheet_destination_point_name);
+        TextView date = (TextView) rootView.findViewById(R.id.bottom_sheet_fare_date);
+
+        startPoint.setText(findLocation(fareRequest.getStartingPoint()));
+        endPoint.setText(findLocation(fareRequest.getEndingPoint()));
+        date.setText(getTimeFromDate(fareRequest.getStartingDate()));
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
         fareAcceptBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
-                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
                     fareAcceptBtn.setText("dupa");
-                }
-                else {
-                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                    fareAcceptBtn.setText("dupa");
-                }
             }
         });
     }
@@ -97,5 +125,7 @@ public class AvailableJourneysFragment extends Fragment {
 
     public void onFareRequested(CMFareRequest fareRequest) {
         Log.d("AvailableJourneys", fareRequest.toString());
+        openBottomSheet(rootView, bottomSheetBehavior, fareRequest);
+
     }
 }
